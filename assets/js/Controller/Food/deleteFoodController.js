21d1bf9ff4306cfad2174cc24d7ef3a1dcb78c83
Nithyanandam <divakarvenu26@gmail.com>
2015-04-10 00:10:19 +0530

    done sample

--- assets/js/Controller/Food/deleteFoodController.js
+++ assets/js/Controller/Food/deleteFoodController.js
@@ -1,4 +1,4 @@
-foodModule.controller('FoodListController', function($scope,$resource,$state,$window,Notes,localStorageService){
+foodModule.controller('FoodListController', function($scope,$resource,$state,Notes,localStorageService){
 
 //  var User=$resource('http://localhost:1337/User/:email', {email:'@email'});
 //  $scope.user=User.get({email:localStorageService.get('user').email});
@@ -14,6 +14,10 @@ foodModule.controller('FoodListController', function($scope,$resource,$state,$wi
 	 console.log("localstorage");
 	 console.log(localStorageService.get('user'));
 
+  $scope.chec=function(value){
+    console.log(value);
+    alert(value);
+  }
 
 	$scope.deleteFood = function (Food,element) {
 			if(confirm("Are you sure u want to delete?")) {
