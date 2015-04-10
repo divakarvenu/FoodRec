7514afd7a79e18ffa99c5aae8d7dac8549bec4d3
nithyanandamv <divakarvenu26@gmail.com>
2015-04-10 17:56:55 +0530

    changes in the upvote downvote

--- assets/js/Controller/Food/viewFoodController.js
+++ assets/js/Controller/Food/viewFoodController.js
@@ -1,19 +1,31 @@
 foodModule.controller('viewFoodController', function($scope,$stateParams,Notes,localStorageService){
 
+
  console.log("localstorage");
  console.log(localStorageService.get('user'));
   console.log($stateParams.id)
-	$scope.scratch = Notes.get({id:$stateParams.id});			//get scratch with id in state paremeters
+	$scope.scratch = Notes.get({id:$stateParams.id}).$promise.then(function(todos) {
+    $scope.scratch=todos;
+    console.log('working is awesome');
+    console.log(todos.owner.id);
+    console.log($scope.scratch.owner.id);
+    flag=$scope.scratch;
+});			//get scratch with id in state paremeters
+
+
+
 
   $scope.upvote=function(){
     console.log('inside function');
-    console.log($scope.scratch.foodupvote);
     if($scope.scratch.foodupvote){
-      $scope.scratch.foodupvote=$scope.scratch.foodupvote+1;
-   }
-   else{
+        $scope.scratch.foodupvote=$scope.scratch.foodupvote+1;
+      }
+    else{
      $scope.scratch.foodupvote=1;
-   }
+      }
+
+
+
    $scope.scratch.$save(function () {
      console.log('upvote');
      });
